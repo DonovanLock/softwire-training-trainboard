8c068f18483cee39611740444a779c1aea7b1512
Alex Mann <Alex.Mann@softwire.com>
2025-07-09 14:24:07 +0100

    single API call works

--- src/app/station/[stationId]/page.tsx
+++ src/app/station/[stationId]/page.tsx
@@ -1,21 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+async function getStationDetails(stationId : string) {
+    if (!process.env.BASE_URL) {
+        throw new Error('No base URL provided.')
+    } else if (!process.env.API_KEY) {
+        throw new Error('No API KEY provided.')
+    }
+
+    const APIURL = process.env.BASE_URL + "stationDetails/" + stationId;
+    console.log(APIURL);
+    const res = await fetch(APIURL, {
+        headers: {
+            "x-api-key": process.env.API_KEY
+        }
+    });
+    const data = await res.json();
+    return data;
+}
+
 export default async function Page({params}: {params: Promise<{ stationId: string }>}) {
   const { stationId } = await params;
-  const stationCodes = [stationId];
+  const stationData = await getStationDetails(stationId);
+  console.log(stationData);
   return (
         <>
             <div className={"w-full text-center bg-red-800"}>
                 <h1 className={"text-3xl py-3 text-white"}>TrainBoard</h1>
             </div>
             <div>
-                I&apos;m a simple train board, short and lacking innovation.
+                Welcome to the details page for {stationId}
             <div
                 className="">
-                {stationCodes.map((stationName) => (
-                    <div className={"flex flex-row gap-4"} key={stationName}>
-                        <div>Station Code:</div>
-                        <div>{stationName}</div>
-                    </div>
-                ))}
             </div>
             </div>
         </>
