0bb1237cee2fa1948a479991e1fb3598d23ed640
Donovan Lock <Donovan.Lock@softwire.com>
2025-07-11 09:00:04 +0100

    started template for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,15 @@
+import { fetchFromAPI } from "@/app/apiFetch";
+import { formatCamelCase, getTimeFromDateTimeString } from "@/app/helperFunction";
+
+type StationNamesResponse = {
+    stations: { crs: string, name: string }[]
+}
+type StationNames = { crs: string, name: string, id: string }[]
+
+// get list of stations, filter only to those with both name and crs defined
+// (how to handle names/crs codes being repeated?)
+
 
 export default function Home() {
-
-    const stationCodes = ["KGX","EDB"];
-    // test
-    return (
-        <>
-            <div className={"w-full text-center bg-red-800"}>
-                <h1 className={"text-3xl py-3 text-white"}>TrainBoard</h1>
-            </div>
-            <div>
-                I&apos;m a simple train board, short and lacking innovation.
-            <div
-                className="">
-                {stationCodes.map((stationName) => (
-                    <div className={"flex flex-row gap-4"} key={stationName}>
-                        <div>Station Code:</div>
-                        <div>{stationName}</div>
-                    </div>
-                ))}
-            </div>
-            </div>
-        </>
-    );
+    
 }
