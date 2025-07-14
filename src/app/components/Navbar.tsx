833756907e24771559adea76605a25bd74590472
Alex Mann <Alex.Mann@softwire.com>
2025-07-14 15:01:06 +0100

    New home page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default async function Navbar() {
     return (
         <div className="w-full text-center flex" style={{ backgroundColor: "#ce132e" }}>
-            <div className="w-14 p-1 pl-2 pt-2">
+            <div className="w-18 p-1 pl-2 pt-2">
                 <Link href="/">
                     <Image
                         src="/images/HomeButton.png"
@@ -23,7 +23,7 @@ export default async function Navbar() {
             <div className="flex flex-col items-center justify-center w-full">
                 <h1 className="text-3xl py-3 text-white">TrainBoard</h1>
             </div>
-            <div className="w-20 p-1">
+            <div className="w-30 p-1">
                 <Link href="https://www.lner.co.uk/">
                     <Image
                         src="/images/LnerLogo.png"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
     return (
-        <>
-            
-        </>
+        <div>
+            <Image
+                src="/images/HomePage.png"
+                width="10000"
+                height="10000"
+                alt="Home page"
+            />
+        </div>
     );
 }
