cb1a3a6becde6225f1fc7bb9e61b69d5ce1c4039
Donovan Lock <Donovan.Lock@softwire.com>
2025-07-14 14:22:25 +0100

    Added LNER logo to Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default async function Navbar() {
     return (
-        <div className="w-full text-center bg-red-800 flex">
+        <div className="w-full text-center flex" style={{ backgroundColor: "#ce132e" }}>
             <div className="w-14 p-1 pl-2 pt-2">
                 <Link href="/">
                     <Image
@@ -16,6 +16,16 @@ export default async function Navbar() {
             </div>
             <div className="flex flex-col items-center justify-center w-full">
                 <h1 className="text-3xl py-3 text-white">TrainBoard</h1>
+            </div>
+            <div className="w-20 p-1">
+                <Link href="https://www.lner.co.uk/">
+                    <Image
+                        src="/images/LnerLogo.png"
+                        width={50}
+                        height={50}
+                        alt="LNER button"
+                    />
+                </Link>
             </div>
         </div>
     );
